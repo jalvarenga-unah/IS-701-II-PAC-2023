857f1b8873a518d668ca2626f685f1040e59ed89
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2023-09-20 17:46:37 -0600

    Primera clase, intro de python

---
+++ intro-python/main.py
@@ -0,0 +1,19 @@
+print('Hola mundo') 
+print('Mi primer programa en python')
+
+nombre = 'Juan'
+edad = 29
+conDecimales = 20.12
+numeroComplejo = 1j
+sinValor = None
+esMAyor = True
+
+x, y, z = 'Valor1', 'Valor2', 'Valor3'
+
+a = b = c = 'algo....'
+
+# Esto no tiene efecto en la compilación
+# print('Mi nombre es ' + nombre+ ' tengo '+ str(edad) + ' años')
+print(f'Mi nombre es {nombre} tengo {edad} años')
+print(x, y, z)
+
